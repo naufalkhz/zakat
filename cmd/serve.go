5360156fdd398521b9ba60222647b319026d47b4
[naufal khawarozni] <[naufal.khawarozni@gmail.com]>
2023-07-31 01:41:30 +0700

    adding location to local db

--- cmd/serve.go
+++ cmd/serve.go
@@ -40,7 +40,7 @@ var serveCmd = &cobra.Command{
 		}
 
 		// Get Connection
-		connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+		connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 		db, err := utils.GetConnDB(connString)
 		if err != nil {
 			log.Fatal("Error get connection to DB")
